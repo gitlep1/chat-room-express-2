42c01c472a681ec48f4244e8ea269b76318cd600
ashivs2500 <antonioshivers2500@yahoo.com>
2020-12-14 02:13:59 -0500

    will it work

--- curl-scripts/auth/change-password.sh
+++ curl-scripts/auth/change-password.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-API="https://mychatroomm.herokuapp.com"
+API="http://localhost:4741"
 URL_PATH="/change-password"
 
 curl "${API}${URL_PATH}/" \
--- curl-scripts/auth/sign-in.sh
+++ curl-scripts/auth/sign-in.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-API="https://mychatroomm.herokuapp.com"
+API="http://localhost:4741"
 URL_PATH="/sign-in"
 
 curl "${API}${URL_PATH}" \
